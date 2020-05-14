bf498e0eb771920d3a7d1b8f0d22dbab3ff17f74
Ignas Bernotas <ignas@iber.lt>
2020-05-14 17:05:27 +0300

    Uncomment push

--- deploy.sh
+++ deploy.sh
@@ -24,5 +24,5 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
