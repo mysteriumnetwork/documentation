de495d58dd8e66a02b9cbec693f71c3260b10c01
Ignas Bernotas <ignas@iber.lt>
2021-01-08 20:21:11 +0200

    Revert to the DocSearch

--- src/gatsby-theme-apollo-docs/components/header-button.js
+++ src/gatsby-theme-apollo-docs/components/header-button.js
@@ -1,12 +1,1 @@
-// global docsearch
-export default () => {
-  if(typeof window !== 'undefined' && typeof window.docsearch !== 'undefined') {
-    let docSearch = window.docsearch
-    window.docsearch = (options) => {
-      options.appId = 'LOISNC3H67'
-      return docSearch(options)
-    }
-  }
-
-  return null
-};
+export default () => null;
