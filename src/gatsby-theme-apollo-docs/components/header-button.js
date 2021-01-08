0839b0c3cfae9ede37bdd110c9477404df91b4f9
Ignas Bernotas <ignas@iber.lt>
2021-01-08 11:20:48 +0200

    Overwrite algolia appId

--- src/gatsby-theme-apollo-docs/components/header-button.js
+++ src/gatsby-theme-apollo-docs/components/header-button.js
@@ -1,1 +1,12 @@
-export default () => null;
+// global docsearch
+export default () => {
+  if(typeof window.docsearch !== 'undefined') {
+    let docSearch = window.docsearch
+    window.docsearch = (options) => {
+      options.appId = 'LOISNC3H67'
+      return docSearch(options)
+    }
+  }
+
+  return null
+};
