c23d3b8e91711c52c50dfd48015c1a6d2c3b5d2f
etherunit <60128746+etherunit@users.noreply.github.com>
2021-02-26 11:52:54 +0200

    Added payout menu link in sidebar

--- navigation/sidebar.js
+++ navigation/sidebar.js
@@ -16,6 +16,7 @@ module.exports = {
     "node-runners/node-migration",
     "node-runners/service-management",
     "node-runners/service-monitoring",
+    "node-runners/payout-guide",
     "node-runners/troubleshooting/index"
   ],
   "Legal": [
